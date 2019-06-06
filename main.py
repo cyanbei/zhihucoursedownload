1b2ce716f8c773a95d3207b4979cb2ee891c00c9
Yanbei Chaw <adrian@tju.edu.cn>
2019-06-06 20:23:02 +0800

    support video live type
    support video live type

--- main.py
+++ main.py
@@ -195,8 +195,73 @@ class alive(object):
         return ("%d:%02d:%02d" % (h, m, s))
 
 
+class live(object):
+    def __init__(self, liveid):
+        self.id = liveid
+
+    def parse(self):
+        self.outline = self.getoutline()
+        self.projectname = self.outline["subject"]
+        if os.path.exists(self.projectname):
+            pass
+        else:
+            os.mkdir(self.projectname)
+        self.livedl()
+        self.getdescription()
+
+    def getoutline(self):
+        url = "https://api.zhihu.com/lives/" + self.id
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+        }
+        cookies = '_zap=242cfd18-38a0-4797-bb6a-5182f4e77b33; d_c0="AEDoQbB5aQ6PTsONx3RCit7KIltICFPXFTY=|1540376499"; _xsrf=x2AazYI6mTtymh3mMjAilAWlz472dIwB; __gads=ID=ce46cfc8a9772a2a:T=1543237561:S=ALNI_MYZ7iWziEMk4dVWpl1fY-Qg0qqYRA; capsion_ticket="2|1:0|10:1557710315|14:capsion_ticket|44:Y2JlYmZhZWZmNzI3NDBjMWIyNTk3YmY3ZjA5ZDE1MTg=|190684ce84dd9d9f7d22a535dab905fc8e39c438875129a17683c0367c66d93d"; z_c0="2|1:0|10:1557710317|4:z_c0|92:Mi4xTGgxYUFBQUFBQUFBUU9oQnNIbHBEaVlBQUFCZ0FsVk43UlBHWFFCLW9zZXdyU1NRZV9zeWItWU90RmpnU29OWThB|8cb34c292edefd3e9640658a34174433e1b69b12725673c77e0d4fb11b1e4de0"; q_c1=1a884fa247d349b1a8ec5754371a9bcd|1557710392000|1540376500000; __utmv=51854390.100-1|2=registration_date=20140513=1^3=entry_date=20140513=1; tst=h; __utma=51854390.709010318.1558253844.1559572267.1559780681.3; __utmz=51854390.1559780681.3.3.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/hot; tgw_l7_route=73af20938a97f63d9b695ad561c4c10c'
+        jar = requests.cookies.RequestsCookieJar()
+        for cookie in cookies.split(';'):
+            key, value = cookie.split('=', 1)
+            jar.set(key, value)
+        ol = json.loads(requests.get(url, headers=headers, cookies=jar).content)
+        return ol
+
+    def livedl(self):
+        segmentname = "%s.mp4" % self.projectname
+        segmenturl = self.outline["video"]["formal_video_tape"]["hls_video_url"]
+        print("-" * 50)
+        print("开始下载%s" % segmentname)
+        """
+        这里有问题
+        ff = input("是否调用N_m3u8DL-CLI下载文件？（不调用输入N）")
+        if ff == "N":
+            print("视频下载地址为：%s\n视频名称为：%s\n请使用m3u8下载器下载。" % (segmenturl, segmentname))
+        else:
+            os.system('N_m3u8DL-CLI_v1.5.1.exe "%s" --workDir "%s" --saveName "%s" --startIndex 0' % (segmenturl, self.projectname, segmentname))
+        """
+        #暂时下载m3u8然后拖入N_m3u8DL-CLI_v1.5.1
+        print("视频下载地址为：%s\n视频名称为：%s\n请使用m3u8下载器下载。" % (segmenturl, segmentname))
+        #print("m3u8文件下载完成。")
+        print("-" * 50)
+
+    def getdescription(self):
+        authors = ""
+        authors += "[img]" + self.outline["speaker"]["member"]["avatar_url"].replace("_r", "") + "[/img]\n"  # 头像
+        authors += "[b][size=4]主讲人：" + self.outline["speaker"]["member"]["name"] + "[/size][/b]\n"
+        authors += "主讲人简介：" + self.outline["speaker"]["description"] + "\n"
+
+        briefdes = "[b][size=4]Live讲座简介[/size][/b]"
+        content = self.outline["description_html"] + "\n" + "[b][size=4]内容大纲[/size][/b]\n" + self.outline["outline"]
+        content = content.replace("<p>", "").replace("</p>", "")
+        content = content.replace("<strong>", "[b]").replace("</strong>", "[/b]")
+        content = content.replace("<code>", "").replace("</code>", "")
+        content = content.replace("<pre>", "").replace("</pre", "")
+        content = content.replace('<img src="', "[img]").replace('" alt="', "[/img]")
+        content = content.replace('">', "")
+        briefdes += content
+
+        print(authors)
+        print(briefdes)
+
+
 if __name__ == "__main__":
-    zhihu = input("请输入需要下载的知乎资源类型:\n1.视频私家课\n2.音频私家课\n3.Live（音频）\n4.电子书\n")
+    zhihu = input("请输入需要下载的知乎资源类型:\n1.视频私家课\n2.音频私家课\n3.Live（视频）\n4.Live（音频）\n5.电子书\n")
     if zhihu == "1":
         sjkid = input("请输入要进行下载的知乎音频私家课id:")
         task = sijiake(sjkid)
@@ -206,9 +271,13 @@ if __name__ == "__main__":
         task = asijiake(sjkid)
         task.parse()
     if zhihu == "3":
+        liveid = input("请输入要进行下载的知乎视频Liveid:")
+        task = live(liveid)
+        task.parse()
+    if zhihu == "4":
         liveid = input("请输入要进行下载的知乎音频Liveid:")
         task = alive(liveid)
         task.parse()
-    if zhihu == "4":
+    if zhihu == "5":
         #知乎电子书有drm，暂未找到解密方法
         pass
